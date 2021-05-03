2a47c3745d91b74ba478b62f6d6345d1a5ec23ef
sev291177 <sedi.at.home@gmail.com>
2021-05-03 21:24:47 +0200

    update the code FareCalculatorService in order that the unit tests are all ok

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,31 +1,39 @@
 package com.parkit.parkingsystem.service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	public void calculateFare(Ticket ticket) {
+		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+		}
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+		LocalDateTime inTime = LocalDateTime.of(ticket.getInTime().getYear() + 1900, ticket.getInTime().getMonth() + 1,
+				ticket.getInTime().getDay() + 1, ticket.getInTime().getHours(), ticket.getInTime().getMinutes(),
+				ticket.getInTime().getSeconds());
+		LocalDateTime outTime = LocalDateTime.of(ticket.getOutTime().getYear() + 1900,
+				ticket.getOutTime().getMonth() + 1,
+				ticket.getOutTime().getDay() + 1, ticket.getOutTime().getHours(), ticket.getOutTime().getMinutes(),
+				ticket.getOutTime().getSeconds());
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+		float duration = (float) (ChronoUnit.MINUTES.between(inTime, outTime) / 60.0);
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
+		switch (ticket.getParkingSpot().getParkingType()) {
+		case CAR: {
+			ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+			break;
+		}
+		case BIKE: {
+			ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unkown Parking Type");
+		}
+	}
 }
